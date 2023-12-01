98c817a750ac13dbe03fd2ce0c02b80a79c3e21c
sbmTRT <thureintun@signalbase.co.jp>
2023-12-01 17:56:19 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <h2>Home Page</h2>
-    <!-- Home page content goes here -->
+    <iframe
+      srcdoc="adminHtml"
+      style="width: 100%; height: 500px; border: none;"
+    ></iframe>
   </div>
 </template>
 
 <script>
+import adminHtml from './adminlte/index.html';
+
 export default {
+  data() {
+    return {
+      adminHtml: adminHtml,
+    };
+  },
   // View-specific logic goes here
 }
 </script>
