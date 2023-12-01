4f4dec20bfaf558807a8c9a38f6aebdc3fad2565
sbmTRT <thureintun@signalbase.co.jp>
2023-12-01 19:11:19 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+      </ul>
+
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- User Account Menu -->
+        <li class="nav-item dropdown user-menu">
+          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+            <img src="user-avatar.jpg" class="user-image img-circle elevation-2" alt="User Image">
+            <span class="d-none d-md-inline">John Doe</span>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <!-- User image -->
+            <li class="user-header bg-primary">
+              <img src="user-avatar.jpg" class="img-circle elevation-2" alt="User Image">
+              <p>
+                John Doe - Web Developer
+                <small>Member since Jan. 2012</small>
+              </p>
+            </li>
+            <!-- Menu Footer-->
+            <li class="user-footer">
+              <a href="#" class="btn btn-default btn-flat">Profile</a>
+              <a href="#" class="btn btn-default btn-flat float-right">Sign out</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+
     <!-- Sidebar -->
-    <aside class="main-sidebar">
-      <!-- Sidebar content goes here -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="#" class="brand-link">
+        <img src="logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminLTE</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add your sidebar menu items here -->
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">
+                <i class="nav-icon fas fa-home"></i>
+                <p>Home</p>
+              </router-link>
+            </li>
+            <!-- Add more menu items as needed -->
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
     </aside>
 
     <!-- Content Wrapper -->
     <div class="content-wrapper">
-      <h2>Home Page</h2>
-      <!-- Home page content goes here -->
+      <router-view></router-view>
     </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To be customized based on your needs -->
+      <div class="float-right d-none d-sm-inline">
+        Version 1.0
+      </div>
+    </footer>
   </div>
 </template>
-
 
 <style scoped>
 /* View-specific styles go here */
@@ -22,7 +88,5 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/css/adminlte.min.css">
 
 <!-- Include AdminLTE JS from CDN -->
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/js/adminlte.min.js">
-export default {
-  // View-specific logic goes here
-}</script>
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/js/adminlte.min.js"></script>
+
