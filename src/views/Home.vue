bdbf56cde1cecff86de47f72d7749b41d7dcabc9
sbmTRT <thureintun@signalbase.co.jp>
2023-12-01 18:18:21 +0900

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <h2>Home Page</h2>
-    <!-- Home page content goes here -->
+    <!-- Include the AdminLTE HTML content as a string -->
+    <div v-html="adminHtml"></div>
   </div>
 </template>
 
 <script>
+// Import the HTML content as a string
+import adminHtml from 'raw-loader!./adminLTE/index.html';
+
 export default {
-  // View-specific logic goes here
+  data() {
+    return {
+      adminHtml: adminHtml,
+    };
+  },
+  // Other Vue options go here
 }
 </script>
 
