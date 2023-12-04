1d2deef5ea03407311c162f0c1851fcfea5abb83
sbmTRT <thureintun@signalbase.co.jp>
2023-12-04 12:13:13 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,96 @@
 <template>
-  <div id="app" class="wrapper">
-    <Header />
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <section class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1>Dashboard</h1>
-            </div>
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+      </ul>
+
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
+        <!-- Add your custom components or links here -->
+      </ul>
+    </nav>
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="#" class="brand-link">
+        <img src="./assets/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminLTE</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="./assets/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+            <a href="#" class="d-block">User Name</a>
           </div>
         </div>
-      </section>
 
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add your menu items here -->
+            <li class="nav-item">
+              <router-link to="/dashboard" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>Dashboard</p>
+              </router-link>
+            </li>
+            <!-- Add more menu items as needed -->
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
       <!-- Main content -->
-      <section class="content">
+      <div class="content">
         <div class="container-fluid">
-          <!-- Your dashboard content goes here -->
+          <!-- Your page content goes here -->
+          <router-view></router-view>
         </div>
-      </section>
+        <!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
     </div>
-    <Footer />
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar (optional) -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Add your custom components or settings here -->
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- Add your footer content here -->
+    </footer>
   </div>
+  <!-- ./wrapper -->
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
 import 'admin-lte/dist/css/adminlte.min.css';
 import 'admin-lte/dist/js/adminlte.min.js';
 
 export default {
-  components: {
-    Header,
-    Footer,
-    Home,
-    About,
-  },
-  data() {
-    return {
-      currentView: 'home', // Set the default view
-    };
-  },
-  // App-level logic goes here
-}
+  name: 'AdminLTE',
+};
 </script>
 
 <style>
-/* You can add your custom styles here */
-
-/* Sidebar styles */
-.sidebar {
-  /* Add your styles for the sidebar here */
-}
-
-/* Main content styles */
-.content-wrapper {
-  /* Add your styles for the main content here */
-}
-
-/* Header styles */
-.main-header {
-  /* Add your styles for the header here */
-}
-
-/* Footer styles */
-.main-footer {
-  /* Add your styles for the footer here */
-}
-
-/* Additional styles based on your customization needs */
+/* Add your custom styles here */
 </style>
