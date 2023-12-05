4d71fdba6afdae78cb1256449f9100572f01fd26
sbmTRT <thureintun@signalbase.co.jp>
2023-12-05 16:59:15 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,46 +1,8 @@
-// Import Vue and Vue Router
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-
-// Import components
-import Home from './views/Home.vue';
-import About from './views/About.vue';
-import Selection from './views/Selection.vue';
+// main.js
+import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-
-// Route settings
-const routes = [
-    {
-        path: '/',
-        redirect: '/selection',
-        store,
-    },
-    {
-        path: '/selection',
-        name: 'Selection',
-        component: Selection,
-        store,
-    },
-    {
-        path: '/about',
-        name: 'About',
-        component: About,
-        store,
-    },
-    {
-        path: '/home',
-        name: 'Home',
-        component: Home,
-        store,
-    }
-];
-
-// Create a router instance
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+import router from './routers/router';
 
 // Create a Vue app instance with the router
 const app = createApp(App); // Use the imported App component directly
