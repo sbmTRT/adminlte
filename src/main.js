0f099eacebe2c30e767a429c3a667dec55f81e0d
sbmTRT <thureintun@signalbase.co.jp>
2023-12-05 16:59:58 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-// main.js
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './routers/router';
 
-// Create a Vue app instance with the router
-const app = createApp(App); // Use the imported App component directly
+// Create a Vue app instance with the router and store
+const app = createApp(App);
+app.use(store);
 app.use(router);
 
 // Mount the app to the element with id 'app'
