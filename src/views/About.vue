d88f26522d0e8a8850464fa9486b072f3380e018
sbmTRT <thureintun@signalbase.co.jp>
2023-12-05 17:37:02 +0900

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -34,19 +34,26 @@
             </div>
             <!-- /.info-box-content -->
           </div>
-            <!-- /.info-box -->
-            <div class="info-box shadow  mt-4">
-              <div class="row">
-                <h2>About Page</h2>
-              </div>
-              <div class="row">
-                <div v-if="submittedData">
-                  <p>User ID: {{ submittedData.userId }}</p>
-                  <p>Password: {{ submittedData.password }}</p>
+          <div class="info-box shadow  mt-4">
+            <span class="info-box-icon bg-warning">
+              <i class="fas fa-wrench"></i>
+            </span>
+            <div class="info-box-content">
+              <span class="info-box-text text-sm">
+                <div class="container-fluid">
+                  <div class="row">
+                  <div v-if="submittedData">
+                    <div class="col-sm-auto"><b>【User ID】</b> {{ submittedData.userId }}</div>
+                    <div class="col-sm-auto"><b>【Password】</b> {{ submittedData.password }}</div>
+                    <div class="col-sm-auto"><b>【Line User ID】</b> <p v-if="userid">{{ userid }}</p></div>
+                    <div class="col-sm-auto"><b>【Line User Name】</b> <p v-if="diaplayname">{{ diaplayname }}</p></div>
+                  </div>
+                  </div>
                 </div>
-              </div>
+              </span>
             </div>
-          <!-- /.info-box -->
+            <!-- /.info-box-content -->
+          </div>
         </div>
         <!-- /.container-fluid -->
       </div>
@@ -59,12 +66,52 @@
 </template>
 
 <script>
+import liff from "@line/liff";
+
 export default {
   computed: {
     submittedData() {
       return this.$store.state.formData;
     },
   },
+  data() {
+    return {
+      userid: "",
+      message: "",
+      error: "",
+      displayname: "",
+      pictureurl: "",
+      statusmessage: ""
+
+    };
+  },
+  mounted() {
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID
+      })
+      .then(() => {
+        this.message = "LIFF init succeeded.";
+        if (liff.isLoggedIn()) {
+          // Get user profile
+          liff.getProfile().then((profile) => {
+            const userId = profile.userId;
+            const displayName = profile.displayName;
+            const pictureUrl = profile.pictureUrl;
+            const statusMessage = profile.statusMessage;
+            this.userid = userId;
+            this.diaplayname = displayName;
+          }).catch((error) => {
+            console.error('Error getting user profile', error);
+          });
+        } else {
+          this.userid = 'User ID: empty';
+        }
+      }).catch((e) => {
+        this.message = "LIFF init failed.";
+        this.error = `${e}`;
+      });
+  }
 };
 </script>
 
